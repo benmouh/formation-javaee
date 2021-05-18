2ceab070770f9d35f1d9d2c3b58ee5991ec01831
lhoussaine <aitbenmouh@yahoo.fr>
2021-05-18 12:44:31 +0200

    hello world EJB service

---
+++ traingin-cdi/formation/ejb/helloservice/src/main/java/formation/helloservice/ejb/HelloServiceBean.java
@@ -0,0 +1,24 @@
+
+package formation.helloservice.ejb;
+
+import javax.ejb.Stateless;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+/**
+ * HelloServiceBean is a web service endpoint implemented as a stateless
+ * session bean.
+ */
+
+@Stateless
+@WebService
+public class HelloServiceBean {
+    private final String message = "Hello, EJB WS";
+    
+    public void HelloServiceBean() {}
+    
+    @WebMethod
+    public String sayHello(String name) {
+        return message + name + ".";
+    }
+}
